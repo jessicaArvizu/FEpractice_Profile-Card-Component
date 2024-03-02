ac1de64dcf23239be76a9dc712a312e4dabb6beb
Jessica Arvizu <jessica.arvizuto@gmail.com>
2024-03-01 20:09:41 -0600

    Backgrounds

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./main.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-cyan': 'hsl(185, 75%, 39%)',
+        'very-dark-blue': 'hsl(229, 23%, 23%)',
+        'dark-grayish-blue': 'hsl(227, 10%, 46%)',
+        'dar-gray':'hsl(0, 0%, 59%)'
+      },
+      backgroundImage:{
+        'back-top':'url(/images/bg-pattern-top.svg)',
+        'back-bottom':'url(/images/bg-pattern-bottom.svg)',
+      }
+    },
   },
   plugins: [],
 }
