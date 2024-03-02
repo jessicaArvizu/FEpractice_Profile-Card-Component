dd24d9bdc7adb1a70f2d83f75bbc42eb3e6539a8
Jessica Arvizu <jessica.arvizuto@gmail.com>
2024-03-01 22:05:45 -0600

    Updated README.md

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         'dark-cyan': 'hsl(185, 75%, 39%)',
         'very-dark-blue': 'hsl(229, 23%, 23%)',
         'dark-grayish-blue': 'hsl(227, 10%, 46%)',
-        'dar-gray':'hsl(0, 0%, 59%)'
+        'dark-gray':'hsl(0, 0%, 59%)'
       },
       backgroundImage:{
         'back-top':'url(/images/bg-pattern-top.svg)',
